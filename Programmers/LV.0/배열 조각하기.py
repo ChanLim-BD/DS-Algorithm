c5b1851f34f55c2d979e9fe293b553dcb4b2c482
Chan <linsay070@gmail.com>
2023-05-30 19:35:01 +0900

    .

--- Programmers/LV.0/배열 조각하기.py
+++ Programmers/LV.0/배열 조각하기.py
@@ -1,7 +1,15 @@
 def solution(arr, query):
     for k, q in enumerate(query):
+        print(k, q)
         if k % 2 == 0:
             arr = arr[:q + 1]
         else:
             arr = arr[q:]
     return arr
+
+def main():
+    print(solution([0, 1, 2, 3, 4, 5], [4, 1, 2]))
+
+
+if __name__ == '__main__':
+    main()
