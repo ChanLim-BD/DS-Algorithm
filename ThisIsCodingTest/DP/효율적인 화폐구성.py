4b619157234d7386d021aa9051e5cbf4a285efa5
Chan <linsay070@gmail.com>
2023-10-14 00:03:17 +0900

    .

---
+++ ThisIsCodingTest/DP/효율적인 화폐구성.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+lst = []
+for _ in range(N):
+    lst.append(int(input()))
+
+dp = [10001] * (M + 1)
+
+# print(lst)
+dp[0] = 0
+for i in range(N):
+    for j in range(lst[i], M + 1):
+        if dp[j - lst[i]] != 10001:
+            dp[j] = min(dp[j], dp[j - lst[i]] + 1)
+
+if dp[M] == 10001:
+    print(-1)
+else:
+    print(dp[M])
